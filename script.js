a6c3ffeefa603735ab420d974163281e3bde1c74
Aleksandr Feshchuk <aleksfeschuk@gmail.com>
2025-07-01 20:44:14 -0600

    writing logical code in script.js

--- script.js
+++ script.js
@@ -5,5 +5,34 @@ document.addEventListener('DOMContentLoaded', () => {
     const confirmedCount = document.getElementById('confirmed-count');
     const clearAllButton = document.getElementById('clear-all');
 
+    let guests = JSON.parse(localStorage.getItem('guestList')) || [];
+    renderGuests();
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const name = guestNameInput.value.trim();
+        if (name) {
+            guests.push({ name, confirmed:false });
+            guests.sort((a, b) => a.name.localeCompare(b.name));
+            saveGuests();
+            renderGuests();
+            guestNameInput.value = ''
+        }
+    });
+
+    clearAllButton.addEventListener('click', () => {
+        guests = [];
+        saveGuests();
+        renderGuests();
+    });
+
+    function saveGuests() {
+        localStorage.setItem('guestList', JSON.stringify(guests));
+    }
+
+    function renderGuests() {
+        guestList.innerHTML = '';
+        let confirmed = 0;
+    }
 
 });
