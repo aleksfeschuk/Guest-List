fc659ad49774e00e27b7275024fe5d1d1c3f9520
Aleksandr Feshchuk <aleksfeschuk@gmail.com>
2025-07-01 20:24:39 -0600

    add style.css, script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('add-guest-form');
+    const guestList = document.getElementById('guest-list');
+    const guestNameInput = document.getElementById('guest-name');
+    const confirmedCount = document.getElementById('confirmed-count');
+    const clearAllButton = document.getElementById('clear-all');
+
+
+});
